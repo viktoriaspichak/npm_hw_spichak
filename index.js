3b53316dad28a5d8f251e3cd55d84655eec7c529
viktoriaspichak <vspichak@ukr.net>
2023-04-07 14:04:23 +0300

    finished

---
+++ index.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+  const result = a + b;
+  return result;
+}
+sum(1, 1);
+function sub(a, b) {
+  const result = a - b;
+  return result;
+}
+sub(1, 1);
+function multi(a, b) {
+  const result = a * b;
+  return result;
+}
+multi(1, 1);
